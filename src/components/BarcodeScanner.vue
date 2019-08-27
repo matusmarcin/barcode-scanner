f8b7483f01b812e64f3f3b5b4b9daa84272b3fd1
Matus Marcin <matus.marcin@gmail.com>
2019-08-27 21:26:16 +0200

    Massive improvements

--- src/components/BarcodeScanner.vue
+++ src/components/BarcodeScanner.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="barcode-scanner">
-    Ukáž EAN!
+    Ahoj! 👋 Klikni ▶️ a ukáž EAN!
     <h1>
       <code>{{code ? code : '...'}}</code>
       <small v-if="code" v-clipboard="code" class="clipboard" @success="copied">📋</small>
     </h1>
-    <div id="scanner"></div>
+    <div id="scanner">
+      <!-- <div id="target">
+        <div id="target-inner">
+        </div>
+      </div> -->
+      <img id="screenshot">
+    </div>
+    <template v-if="isReady">
+      <h2 @click="stop" v-if="isScanning">
+        🛑
+      </h2>
+      <h2 @click="start" v-else-if="!code">
+          ▶️
+      </h2>
+      <p v-else>
+        <a href="#" @click.prevent="reloadPage">Urob to znovu! ⟳</a>
+      </p>
+    </template>
+    <template v-else>
+      <p><b>Povoľ kameru!</b></p>
+    </template>
+    <p><a href="#" @click.prevent="debugShown = !debugShown">Debug ⊞</a></p>
+    <p v-if="debugShown" class="debug">
+      Satisfactory count: <code>{{satisfactoryFoundCount}}</code><br />
+      Codes found (and their count):<br />
+      <pre><code>{{codesFound}}</code></pre>
+    </p>  
   </div>
 </template>
 
@@ -18,16 +44,55 @@ export default {
   },
   data() {
     return {
-      code: ''
-    }
+      code: '',
+      codesFound: {},
+      satisfactoryFoundCount: 10,
+      isReady: false,
+      isScanning: false,
+      debugShown: false,
+      resultCollector: null,
+    };
   },
   mounted() {
+    /*
+      TODO     
+      [x] Use larger resolution
+      [ ] Limit search area better
+      [ ] Maybe 8 workers?
+      [x] Collect barcodes, store in array with count and show the most frequent one
+      [ ] Use image? Take a screenshot and process that.
+      [ ] Re-starting probably needs init again? Video dies.
+
+     */ 
+    this.resultCollector = Quagga.ResultCollector.create({
+        capture: true,
+        capacity: 40,
+        filter: function(codeResult) {
+          // only store results which match this constraint
+          // returns true/false
+          // e.g.: return codeResult.format === "ean_13";
+          return true;
+        }
+    });
+
     Quagga.init({
       inputStream: {
         name: "Live",
         type: "LiveStream",
-        target: document.querySelector('#scanner')    // Or '#yourElement' (optional)
+        target: document.querySelector('#scanner'),    // Or '#yourElement' (optional)
+        singleChannel: false,
+        constraints: {
+          width: 800,
+          height: 600
+        }
+        // area: { // defines rectangle of the detection/localization area
+        //   top: "20%",    // top offset
+        //   right: "20%",  // right offset
+        //   left: "20%",   // left offset
+        //   bottom: "20%"  // bottom offset
+        // }
       },
+      // frequency: 50, // default is 10?
       decoder: {
         readers : ["ean_reader"]
       },
@@ -44,13 +109,19 @@ export default {
             return
         }
         console.log("Initialization finished. Ready to start");
+        Quagga.registerResultCollector(this.resultCollector);
         Quagga.onProcessed(this.maybeFound);
-        Quagga.onDetected(this.found);        
-        Quagga.start();
+        Quagga.onDetected(this.found);
+        this.ready();
     },
     found: function (data) {
       console.log(data);
-      this.code = data.codeResult.code;
+      this.code = this.logCode(data.codeResult.code);
+      if (this.isSatisfactory()) {
+        // TODO Take a screenshot first and replace video
+        this.screenshot();
+        this.stop();
+      }
     },
     maybeFound: function (result) {
         var drawingCtx = Quagga.canvas.ctx.overlay,
@@ -78,6 +149,40 @@ export default {
     },
     copied: function () {
 
+    },
+    logCode: function (code) {
+      if (typeof this.codesFound[code] !== 'undefined') {
+        this.codesFound[code]++;
+      } else {
+        this.codesFound[code] = 1;
+      }
+      return Object.keys(this.codesFound).reduce((mostFrequentCode, currentCode) => {
+        const currentCodeCount = this.codesFound[currentCode];
+        return this.codesFound[mostFrequentCode] < currentCodeCount ? currentCode : mostFrequentCode;
+      }, code);
+    },
+    ready: function () {
+      this.isReady = true;
+    },
+    start: function () {
+      this.code = '';
+      this.codesFound = {};
+      Quagga.start();
+      this.isScanning = true;
+    },
+    stop: function () {
+      this.isScanning = false;
+      Quagga.stop();
+    },
+    isSatisfactory: function () {
+      return this.codesFound[this.code] >= this.satisfactoryFoundCount;
+    },
+    screenshot: function () {
+      const canvas = Quagga.canvas.dom.image;
+      document.getElementById('screenshot').setAttribute('src', canvas.toDataURL());
+    },
+    reloadPage: function () {
+      window.location.reload();
     }
   }
 }
@@ -93,18 +198,48 @@ export default {
 }
 #scanner {
   position: relative;
-  max-height: 480px;
-  max-width: 640px;
+  height: 240px;
+  width: 320px;
   margin: 0 auto;
 }
 #scanner video,
-#scanner canvas {
+#scanner canvas,
+#scanner #target,
+#scanner #screenshot {
   position: absolute;
   top: 0;
   left: 0;
   z-index: 1;
+  width: 320px;
+  height: 240px;
+}
+#scanner #screenshot {
+  z-index: 3;
 }
 #scanner canvas {
-  z-index: 2;
+  z-index: 4;
+}
+#scanner #target {
+  padding: 18% 20% 20%;
+  height: 288px;
+  width: 384px;
+  z-index: 5;
+}
+#scanner #target-inner {
+  /* border: 2px solid red; */
+  height: 284px;
+  width: 380px;
+  z-index: 4;
+}
+.debug {
+  text-align: left;
+  color: #fff;
+  background: #333;
+  padding: 2em;
+}
+h2 {
+  font-size: 3rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
 }
 </style>
